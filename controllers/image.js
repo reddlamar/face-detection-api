440e2ad4243a11e903bf5f573d8658b1f8cdc25c
reddlamar <reddlamar1@gmail.com>
2023-11-13 00:58:19 -0800

    server setup

---
+++ controllers/image.js
@@ -0,0 +1,90 @@
+const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
+
+const stub = ClarifaiStub.grpc();
+
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key 55f948ddd2c94ba5886990a61c71c182");
+
+const handleAPI = (req, res) => {
+
+    stub.PostModelOutputs(
+        {
+            // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
+            model_id: "face-detection",
+            inputs: [{data: {image: {url: req.body.input}}}]
+        },
+        metadata,
+        (err, response) => {
+            if (err) {
+                console.log("Error: " + err);
+                return;
+            }
+    
+            if (response.status.code !== 10000) {
+                console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
+                return;
+            }
+    
+            console.log("Predicted concepts, with confidence values:")
+            for (const c of response.outputs[0].data.concepts) {
+                console.log(c.name + ": " + c.value);
+            }
+
+            res.json(response);
+        }
+    );
+}
+
+const getClarifaiRequestOptions = (req) => { 
+    // Your PAT (Personal Access Token) can be found in the portal under Authentification
+    const PAT = 'efbcfa7162a146adab19f407654eedd3';
+
+    // Specify the correct user_id/app_id pairings
+    // Since you're making inferences outside your app's scope
+    const USER_ID = 'reddlamar';       
+    const APP_ID = 'face-recognition';
+
+    // Change image URL you want to use  
+    const IMAGE_URL = req.body.input;
+
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": IMAGE_URL
+                    }
+                }
+            }
+        ]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+
+    return requestOptions;
+  };
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => res.json(entries[0].entries))
+    .catch(err => res.status(400).json('unable to get entries'));
+};
+
+module.exports = {
+    handleImage,
+    handleAPI
+}
