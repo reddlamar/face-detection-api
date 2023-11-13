cf5d0a065c055bbb923bc65401013b0c5c908141
reddlamar <reddlamar1@gmail.com>
2023-11-13 01:29:29 -0800

    remove build from script and using process.env.PORT

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 
 app.post('/imageURL', (req, res) => {image.handleAPI(req, res)});
 
-app.listen(3000, () => {
-    console.log('App is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`App is running on port ${process.env.PORT}`);
 })
